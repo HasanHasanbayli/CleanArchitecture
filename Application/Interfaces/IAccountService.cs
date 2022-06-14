cae84bcbb6fda02979ca3c11222dd298d1045205
hasanhasanbayli <hasanhasanbeyli@gmail.com>
2022-06-14 17:10:23 +0400

    Add Interfaces

---
+++ Application/Interfaces/IAccountService.cs
@@ -0,0 +1,17 @@
+﻿using Application.DTOs.Account;
+using Application.Wrappers;
+
+namespace Application.Interfaces;
+
+public interface IAccountService
+{
+    Task<Response<AuthenticationResponse>> AuthenticateAsync(AuthenticationRequest request, string ipAddress);
+    
+    Task<Response<string>> RegisterAsync(RegisterRequest request, string origin);
+    
+    Task<Response<string>> ConfirmEmailAsync(string userId, string code);
+    
+    Task ForgotPassword(ForgotPasswordRequest model, string origin);
+    
+    Task<Response<string>> ResetPassword(ResetPasswordRequest model);
+}
---
+++ Application/Wrappers/Response.cs
@@ -0,0 +1,29 @@
+﻿namespace Application.Wrappers;
+
+public class Response<T>
+{
+    public Response()
+    {
+    }
+
+    public Response(T data, bool succeeded, string message = default!)
+    {
+        Succeeded = succeeded;
+        Message = message;
+        Data = data;
+    }
+
+    public Response(bool succeeded, string message = default!)
+    {
+        Succeeded = succeeded;
+        Message = message;
+    }
+
+    public bool Succeeded { get; set; }
+
+    public string Message { get; set; } = null!;
+
+    public List<string> Errors { get; set; } = null!;
+
+    public T Data { get; set; } = default!;
+}
